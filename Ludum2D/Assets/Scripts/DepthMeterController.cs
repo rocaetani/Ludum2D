c3dcb5ed3dbc2191d433bc6e9c2542c324a36a9a
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-24 14:56:39 -0300

    Atualiza medidor de altura com a altura do Jogador

---
+++ Ludum2D/Assets/Scripts/DepthMeterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthMeterController : MonoBehaviour
+{
+    public Transform playerPosition;
+    public float maxDepth = 1;
+
+    public GameObject chevron;
+
+    private RectTransform _depthTransform;
+    private RectTransform _chevronTransform;
+
+    private float _startHeight;
+    private float _endHeight;
+
+    void Start()
+    {
+        _depthTransform = gameObject.GetComponent<RectTransform>();
+        _chevronTransform = chevron.GetComponent<RectTransform>();
+
+        _startHeight = 0;
+        _endHeight = _depthTransform.sizeDelta.y - _chevronTransform.sizeDelta.y;
+    }
+
+    void Update()
+    {
+        float currentDepth = playerPosition.position.y;
+        float depthRatio = currentDepth / maxDepth;
+
+        float interpolatedHeight = (depthRatio * _endHeight) + _startHeight;
+        // interpolatedHeight = Mathf.Min(_startHeight, interpolatedHeight);
+        // interpolatedHeight = Mathf.Max(_endHeight, interpolatedHeight);
+
+        _chevronTransform.anchoredPosition = new Vector3(_chevronTransform.anchoredPosition.x, interpolatedHeight);
+        // chevron.transform.position += new Vector3(0, -Time.deltaTime, 0);
+    }
+}
