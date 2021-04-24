53379033af9a52f747b2a68cbb82b910378c34f2
Rodrigo Caetani <rocaetani@gmail.com>
2021-04-24 00:51:08 -0300

    Falling e Baackground init

---
+++ Ludum2D/Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChange : MonoBehaviour
+{
+    
+    public Camera MainCamera;
+    public Transform PlayerPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerPosition.position.y < -5)
+        {
+            MainCamera.backgroundColor = Color.green;
+        }
+
+    }
+}
---
+++ Ludum2D/Assets/Scripts/PlayerFalling.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFalling : MonoBehaviour
+{
+    public float velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * velocity * Vector3.down;
+    }
+}
