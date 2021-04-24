2fb05d8cfd03147c423ca42c367d1b955565e5b5
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-24 17:39:30 -0300

    Mostra oxigenio sobrando no circulo

---
+++ Ludum2D/Assets/Scripts/OxygenMeterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenMeterController : MonoBehaviour
+{
+
+   private Image _oxygenPie;
+
+    void Start()
+    {
+        _oxygenPie = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        Debug.Log(GameObjectAccess.Player.AirRatio);
+        _oxygenPie.fillAmount = GameObjectAccess.Player.AirRatio;
+    }
+}
