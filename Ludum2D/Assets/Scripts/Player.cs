d49a3236917e2c107dd25ddcb99a76f0ed4be02c
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-24 17:04:30 -0300

    Usa floats para oxigenio no player prefab

--- Ludum2D/Assets/Scripts/Player.cs
+++ Ludum2D/Assets/Scripts/Player.cs
@@ -5,27 +5,30 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    
+
     [Header("Movement")]
     public float Velocity;
 
 
     private Vector3 _direction;
 
-    private int _airBar;    
 
     private int _currentSeconds;
-    
-    [Header("Air Bar")]
-    
-    public int ThresholdSeconds;
 
-    public int AmountToDecrease;
+    [Header("Air Bar")]
+    public float AirMaximum;
+    public float AirAmount;
+    public float AmountToDecrease;
+    public float AirRatio {
+        get {
+            return AirAmount / AirMaximum;
+        }
+    }
 
     void Start()
     {
+        AirAmount = AirMaximum;
         _direction = Vector3.down;
-        _airBar = 100;
     }
 
     // Update is called once per frame
@@ -48,33 +51,19 @@ public class Player : MonoBehaviour
 
     private void LoseAir()
     {
-        if (TruncateTime() - _currentSeconds >= ThresholdSeconds)
-        {
-            _currentSeconds = TruncateTime();
-            DecreaseAirBar();
-        }
-    }
-
-    private void DecreaseAirBar()
-    {
-        _airBar -= AmountToDecrease;
-    }
-
-    private int TruncateTime()
-    {
-        return Mathf.FloorToInt(Time.time);
+        AirAmount -= AmountToDecrease * Time.deltaTime;
     }
 
     void OnGUI()
     {
         GUI.color = Color.red;
-        
-       
-        
-        GUI.Label(new Rect(100, 10, 150, 100), _airBar + "");
-        
+
+
+
+        GUI.Label(new Rect(100, 10, 150, 100), AirAmount + "");
+
         GUI.Label(new Rect(100, 20, 150, 100), Time.time + "");
 
     }
-    
+
 }
