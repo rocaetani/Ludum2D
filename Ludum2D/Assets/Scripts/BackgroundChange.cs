f4cc0bda5ed26464d6f346d3fa7c304dd97137c6
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-24 03:13:14 -0300

    Permite mudança do background

--- Ludum2D/Assets/Scripts/BackgroundChange.cs
+++ Ludum2D/Assets/Scripts/BackgroundChange.cs
@@ -4,23 +4,52 @@ using UnityEngine;
 
 public class BackgroundChange : MonoBehaviour
 {
-    
+
     public Camera MainCamera;
     public Transform PlayerPosition;
-    
-    // Start is called before the first frame update
+
+    public List<float> depthChanges;
+    public List<Color> colorChanges;
+
+    // private Dictionary<float, Color> _backgroundChanges = new Dictionary<float, Color>();
+
     void Start()
     {
-        
+        if(depthChanges.Count != colorChanges.Count)
+        {
+            Debug.LogError("Depth Changes e Color Changes devem ter o mesmo tamanho");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PlayerPosition.position.y < -5)
+        Color from = colorChanges[0];
+        Color to = colorChanges[colorChanges.Count - 1];
+        float interpolateAmount = 0;
+
+        float currentDepth = PlayerPosition.position.y;
+
+        for(int i = depthChanges.Count - 1; i >= 0 ; i--)
         {
-            MainCamera.backgroundColor = Color.green;
+            // Found depth interval
+            if(depthChanges[i] >= currentDepth) {
+                from = colorChanges[i];
+                to = colorChanges[Mathf.Min(colorChanges.Count, i+1)];
+
+                float distanceFromDepth = Mathf.Abs(currentDepth - depthChanges[i]);
+                float distanceBetweenDepths = Mathf.Abs(depthChanges[Mathf.Min(colorChanges.Count, i+1)] - depthChanges[i]);
+                interpolateAmount = distanceFromDepth / distanceBetweenDepths;
+                break;
+            }
         }
 
+        Color interpolatedColor = Color.Lerp(from, to, interpolateAmount);
+
+        Debug.Log($"From: {from}, To: {to}");
+        Debug.Log($"Color: {interpolatedColor}, Amount: {interpolateAmount}");
+
+        MainCamera.backgroundColor = interpolatedColor;
     }
+
 }
