d79b6af95c12de61444ab0ebe6d34d2522924206
11Nim <73611852+11Nim@users.noreply.github.com>
2021-04-25 10:25:05 -0300

    BonecoMudandoACara

---
+++ Ludum2D/Assets/ToChangehisFace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToChangehisFace : MonoBehaviour
+{
+
+    public Animator animator;
+    public float qlqrcoisa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        qlqrcoisa = GameObjectAccess.Player.AirRatio;
+        animator.SetFloat("O2Meter", qlqrcoisa);
+    }
+}
