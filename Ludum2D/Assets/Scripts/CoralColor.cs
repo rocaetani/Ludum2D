8c4d042c2bc6b07a40a833f664d60ce331b2a502
Pedro Ortiz Bledow <kynakerisk@gmail.com>
2021-04-26 00:39:16 -0300

    Cria prefab de coral

---
+++ Ludum2D/Assets/Scripts/CoralColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralColor : MonoBehaviour
+{
+    public bool chooseRandomColor = true;
+    public List<Color> coralPalette;
+
+    private SpriteRenderer _sprite;
+
+    void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _sprite.color = coralPalette[Random.Range(0, coralPalette.Count)];
+    }
+}
